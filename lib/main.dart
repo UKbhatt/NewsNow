c8e53b347b5abb855ea150ff691a535baa33d27e
Utkarsh Bhatt <ubhatt2004@gmail.com>
2025-02-02 11:55:28 +0530

    feat: Created Namedroutes for better understanding of navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:newapp/view/homeScreen.dart';
 import './view/splashScreen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import './view/CategorySection.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -13,13 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/Home' : (context) => const Homescreen(),
+        '/Category' : (context) => const Categorysection() ,
+      },
       title: 'News App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Splashscreen(), 
+      home: const Splashscreen(),
     );
   }
 }
